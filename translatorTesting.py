ea651c0e667e296d3c000d475a29db1fd7965e27
RavenLLevitt <103865865+RavenLLevitt@users.noreply.github.com>
2023-01-26 15:29:03 -0500

    Removing Defunct API Keys

--- translatorTesting.py
+++ translatorTesting.py
@@ -1,5 +1,5 @@
 import deepl
-translator = deepl.Translator("b92d0f2a-2c86-3f15-b9e9-4f44fc65fd02:fx")
+translator = deepl.Translator("INSERT KEY")
 result = translator.translate_text("hello", target_lang="ES") 
 translated_text = result.text
 print(translated_text)
